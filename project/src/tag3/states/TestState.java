b6edfe80591c9c2efa9b719c13067ce2f7c39574
Johnny <horsentp@gmail.com>
2013-10-11 13:38:10 -0500

    I keep forgetting stuff jfoiajgoiwjf

--- project/src/tag3/states/TestState.java
+++ project/src/tag3/states/TestState.java
@@ -1,6 +1,9 @@
 package tag3.states;
 
+import horsentp.display.ImageDisplay;
 import horsentp.gamelogic.GameState;
+import tag3.media.MediaLoader;
+
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
 /**
@@ -12,13 +15,19 @@ import java.awt.Graphics2D;
  */
 public class TestState extends GameState {
 
+    private BufferedImage testImage;
+
     public void updateLogic() {
 
     }
 
+    public void initState() {
+        testImage = MediaLoader.quickLoadImage("testImage.png");
+    }
+
     public BufferedImage render(BufferedImage bImage, Graphics2D g2d) {
         g2d = (Graphics2D) bImage.getGraphics();
-
+        g2d.drawImage(testImage, 0, 0, null);
         return bImage;
     }
 }
