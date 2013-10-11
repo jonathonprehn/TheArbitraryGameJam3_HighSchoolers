e7290f226d568c5b5caeb0220ca50a5c683b6650
Johnny <horsentp@gmail.com>
2013-10-11 12:50:43 -0500

    New library that should be less frustrating to use

--- project/src/tag3/MainStartUp.java
+++ project/src/tag3/MainStartUp.java
@@ -3,6 +3,8 @@ package tag3;
 import horsentp.display.DisplayConfiguration;
 import horsentp.display.DisplayLayer;
 import horsentp.display.Displayer;
+import horsentp.gametime.GameTimer;
+import horsentp.input.InputBridge;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,5 +15,10 @@ public class MainStartUp {
         System.out.println("Lets finish this game or at least start it.");
         DisplayLayer tes = new DisplayLayer();
         tes.addDisplayable(new Checkerboard());
+        Displayer dis = new Displayer(new DisplayConfiguration(800, 600, DisplayConfiguration.STATIC_WINDOW));
+        GameTimer timer = new GameTimer();
+        InputBridge bridge = new InputBridge();
+        bridge.linkToDisplay(dis);
+
     }
 }
