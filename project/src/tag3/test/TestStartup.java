2f613d4089e37137dcf95c66d1f5d990c58f11c0
Kevin <kprehn01@gmail.com>
2013-10-11 18:50:46 -0500

    trying to test things

--- project/src/tag3/test/TestStartup.java
+++ project/src/tag3/test/TestStartup.java
@@ -12,7 +12,7 @@ public class TestStartup {
         System.out.println("Lets finish this game or at least start it.");
         GameRunner runner = new GameRunner(new DisplayConfiguration(800, 600, DisplayConfiguration.STATIC_WINDOW), false);
         GraphicsFactory.getFactory().initFactory(runner.getDisplayer(), runner.getInput());
-        runner.startGame();    ///Don't forget to change the state!
         runner.changeState(new ConsoleMenuState());
+        runner.startGame();    ///Don't forget to change the state!
     }
 }
