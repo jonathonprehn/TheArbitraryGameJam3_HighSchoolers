91cb45bf7d84a7fa58c22355b8395784ebf82d25
Kevin <kprehn01@gmail.com>
2013-10-11 11:05:27 -0500

    Main Class

---
+++ project/src/MainStartUp.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Owner
+ * Date: 10/11/13
+ * Time: 11:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainStartUp {
+
+    public static void main(String[] args) {
+
+    }
+}
