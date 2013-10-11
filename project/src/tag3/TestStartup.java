971e91e5767b2a1991f2a9ce9163c4a9467715d8
Starbuck Johnson <starbuck@modwiz.com>
2013-10-11 17:00:26 -0500

    Added a test startup class.

---
+++ project/src/tag3/TestStartup.java
@@ -0,0 +1,19 @@
+package tag3;
+
+import horsentp.display.DisplayConfiguration;
+import horsentp.gamelogic.GameRunner;
+
+import tag3.states.MainMenuState;
+import tag3.test.states.ConsoleMenuState;
+import tag3.utility.GraphicsFactory;
+
+public class TestStartup {
+
+    public static void main(String[] args) {
+        System.out.println("Lets finish this game or at least start it.");
+        GameRunner runner = new GameRunner(new DisplayConfiguration(800, 600, DisplayConfiguration.STATIC_WINDOW), false);
+        GraphicsFactory.getFactory().initFactory(runner.getDisplayer(), runner.getInput());
+        runner.startGame();    ///Don't forget to change the state!
+        runner.changeState(new ConsoleMenuState());
+    }
+}
