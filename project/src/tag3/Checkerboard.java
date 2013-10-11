f67e55172c62d16c475e053923598505094a419f
Kevin <kprehn01@gmail.com>
2013-10-11 12:15:31 -0500

    Cast!

--- project/src/tag3/Checkerboard.java
+++ project/src/tag3/Checkerboard.java
@@ -13,6 +13,7 @@ import java.awt.*;
 public class Checkerboard extends JComponent {
     @Override
     public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
         g.fillRect(0,0,20,20);
         g.setColor(Color.WHITE);
         g.fillRect(20,0,20,20);
