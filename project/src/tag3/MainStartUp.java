4a5b751b65377ba536418e78271d4c5fe67855c9
Johnny <horsentp@gmail.com>
2013-10-11 12:54:16 -0500

    Lib change again

--- project/src/tag3/MainStartUp.java
+++ project/src/tag3/MainStartUp.java
@@ -5,6 +5,7 @@ import horsentp.display.DisplayLayer;
 import horsentp.display.Displayer;
 import horsentp.gametime.GameTimer;
 import horsentp.input.InputBridge;
+import horsentp.gamelogic.GameRunner;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,12 +14,7 @@ public class MainStartUp {
 
     public static void main(String[] args) {
         System.out.println("Lets finish this game or at least start it.");
-        DisplayLayer tes = new DisplayLayer();
-        tes.addDisplayable(new Checkerboard());
-        Displayer dis = new Displayer(new DisplayConfiguration(800, 600, DisplayConfiguration.STATIC_WINDOW));
-        GameTimer timer = new GameTimer();
-        InputBridge bridge = new InputBridge();
-        bridge.linkToDisplay(dis);
+        GameRunner runner = new GameRunner(new DisplayConfiguration(800, 600, DisplayConfiguration.STATIC_WINDOW));
 
     }
 }
