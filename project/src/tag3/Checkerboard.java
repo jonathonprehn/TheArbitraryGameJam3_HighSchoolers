8ef72dbd92510aeaaec038f0f3ddc9870129bf48
Starbuck Johnson <starbuck@modwiz.com>
2013-10-11 12:09:06 -0500

    Water

---
+++ project/src/tag3/Checkerboard.java
@@ -0,0 +1,24 @@
+package tag3;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Starbuck
+ * Date: 10/11/13
+ * Time: 12:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Checkerboard extends JComponent {
+    @Override
+    public void paintComponent(Graphics g) {
+        g.fillRect(0,0,20,20);
+        g.setColor(Color.WHITE);
+        g.fillRect(20,0,20,20);
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,20,20);
+        g.setColor(Color.WHITE);
+        g.fillRect(20,0,20,20);
+    }
+}
--- project/src/tag3/MainStartUp.java
+++ project/src/tag3/MainStartUp.java
@@ -1,8 +1,14 @@
 package tag3;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainStartUp {
 
     public static void main(String[] args) {
         System.out.println("Lets finish this game or at least start it.");
+        JFrame frame = new JFrame("Checkerboard");
+        frame.add(new Checkerboard(), BorderLayout.CENTER);
+        frame.setVisible(true);
     }
 }
