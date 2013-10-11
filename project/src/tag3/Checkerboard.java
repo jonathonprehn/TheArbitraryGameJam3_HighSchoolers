63f0f7a9d1d41be04cfb0aa4b3bf687105804f9d
Kevin <kprehn01@gmail.com>
2013-10-11 12:16:59 -0500

    derp

--- project/src/tag3/Checkerboard.java
+++ project/src/tag3/Checkerboard.java
@@ -14,12 +14,12 @@ public class Checkerboard extends JComponent {
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        g.fillRect(0,0,20,20);
-        g.setColor(Color.WHITE);
-        g.fillRect(20,0,20,20);
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,20,20);
-        g.setColor(Color.WHITE);
-        g.fillRect(20,0,20,20);
+        g2.fillRect(0,0,20,20);
+        g2.setColor(Color.WHITE);
+        g2.fillRect(20,0,20,20);
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0,0,20,20);
+        g2.setColor(Color.WHITE);
+        g2.fillRect(20,0,20,20);
     }
 }
