d041c9c107646bc6fab4e4b783a0d90b3ff71ea3
Starbuck Johnson <starbuck@modwiz.com>
2013-10-13 16:33:59 -0500

    Made hunter encounters more rare and better

--- project/src/tag3/gamelogic/encounters/HunterEncounter.java
+++ project/src/tag3/gamelogic/encounters/HunterEncounter.java
@@ -18,7 +18,7 @@ import java.util.TimerTask;
 public class HunterEncounter implements RandomEncounter, KeyDownListener{
     @Override
     public double getChancePerHour() {
-        return 30;
+        return 20;
     }
 
     @Override
@@ -36,7 +36,7 @@ public class HunterEncounter implements RandomEncounter, KeyDownListener{
         private PartyWrapper partyWrapper;
 
         private boolean spaceLastUp = true;
-        private int remainingToggles = 10;
+        private int remainingToggles = 4;
         private long lastDownMillis = System.currentTimeMillis();
         private boolean complete = false;
 
@@ -109,6 +109,33 @@ public class HunterEncounter implements RandomEncounter, KeyDownListener{
             public void run() {
                 if (this.dialog.remainingToggles <= 0) {
                     System.out.println("You made it");
+                    gameState.askForConfirmation(new ConfirmCommand() {
+                        @Override
+                        public void preCommandAction() {
+                            gameState.setOtherResourceDialogText("Success");
+                            gameState.setResourceDialogText("You escaped from the hunters!");
+                        }
+
+                        @Override
+                        public void onYes() {
+
+                        }
+
+                        @Override
+                        public void onNo() {
+
+                        }
+
+                        @Override
+                        public boolean isAChoice() {
+                            return false;
+                        }
+                    });
+                    try {
+                        Thread.sleep(500);
+                        // Get their wits about them.
+                    } catch (InterruptedException e) {
+                    }
                     gameState.setAsking(false);
                     gameState.setPressed(true);
                     dialog.setComplete();
