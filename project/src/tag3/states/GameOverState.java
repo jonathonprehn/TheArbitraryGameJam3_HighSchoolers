919fc7313ca04a86929dfb8aa46c7dd816951da5
Starbuck Johnson <starbuck@modwiz.com>
2013-10-13 13:59:24 -0500

    Added a game over state.

---
+++ project/src/tag3/states/GameOverState.java
@@ -0,0 +1,29 @@
+package tag3.states;
+
+import horsentp.gamelogic.GameState;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Starbuck Johnson on 10/13/13.
+ */
+public class GameOverState extends GameState{
+
+    // FILL ME!
+    
+    @Override
+    public void updateLogic() {
+
+    }
+
+    @Override
+    public void initState() {
+
+    }
+
+    @Override
+    public BufferedImage render(BufferedImage bufferedImage, Graphics2D graphics2D) {
+        return null;
+    }
+}
